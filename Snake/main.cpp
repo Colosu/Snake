38f23853dad1b2268702fb35302f6aafde4aeec8
Colosu <alfredocolosu@gmail.com>
2014-07-17 01:00:14 +0200

    Creado Primera Prueba para el Mapa
    Creada una prueba para probar el módulo de mapa.

--- Snake/main.cpp
+++ Snake/main.cpp
@@ -1,2 +1,16 @@
-#include <SDL2/SDL.h>
+#include <iostream>
+using namespace std;
+#include "Mapa.h"
+#include "Presentacion.h"
 
+int main() {
+
+	tMapa *mapa;
+
+	mapa = inicializarMapa();
+	mostrarMapa(*mapa, cout);
+	pausa();
+
+
+	return 0;
+}
